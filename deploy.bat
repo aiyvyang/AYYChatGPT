6d80a0902b90932124f0a481e3ea40f07bb4cfd2
deary <thedanyang@qq.com>
2023-07-17 19:45:26 +0800

    deploy

--- deploy.bat
+++ deploy.bat
@@ -15,6 +15,4 @@ git commit -m 'deploy'
 
 git push -f https://gitee.com/aiyvyang-studio/AYYChatGPT.git main:gh-pages
 
-git push -f https://github.com/aiyvyang-studio/AYYChatGPT.git main:gh-pages
-
 exit
